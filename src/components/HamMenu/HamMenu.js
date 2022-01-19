ba29cf70f8b83fd01be171be39ac401beab4a9ea
Ekaitz <al.ekaitz@gmail.com>
2022-01-19 22:51:18 +0100

    Refactor hamburger menu button -> Remove chakra ui & implement custom buttom with animation

--- src/components/HamMenu/HamMenu.js
+++ src/components/HamMenu/HamMenu.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import './HamMenu.css';
-
 
 
 const HamMenu = () => {
@@ -11,15 +9,11 @@ const HamMenu = () => {
 
 
     return (
-        <div className={`buttonContainer ${!switchIcon ? "" : "adjustPosition"}`}>
-            <button className={`${!switchIcon ? "hamburger" : "close"}`} onClick={handleClick}>
-                {switchIcon === false && (
-                    <HamburgerIcon className="hamburgerIcon"/>
-                    )}
-                {switchIcon === true && (
-                    <CloseIcon className="closeIcon"/>
-                    )}
-            </button>
+        <div className="buttonContainer">
+            <div className={`hamburgerMenu ${switchIcon === true ? "cross" : ""}`} onClick={handleClick}>
+                <div className="bar" id="bar1"></div>
+                <div className="bar" id="bar2"></div>
+            </div>
         </div>
         )
 }
