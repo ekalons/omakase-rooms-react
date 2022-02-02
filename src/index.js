c0923b32a0df177984bc7c45d450c0029b7924b7
Ekaitz <al.ekaitz@gmail.com>
2022-02-02 18:14:40 +0100

    Add dotenv to test deployment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
 import reportWebVitals from './reportWebVitals';
+import 'dotenv/config';
 
 ReactDOM.render(
     <React.StrictMode>
