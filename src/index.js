d3e5eaed3e14419a448d8a836ff761fd4f2ce5c3
Ekaitz <al.ekaitz@gmail.com>
2022-02-02 19:03:22 +0100

    Remove dotenv and prepare for re-deployment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
 import reportWebVitals from './reportWebVitals';
-import 'dotenv/config';
 
 ReactDOM.render(
     <React.StrictMode>
