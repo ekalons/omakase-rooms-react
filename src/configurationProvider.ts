5027d30eb527b35ddbd4b558b476a4701b4aa855
Ekaitz <66970241+ekalons@users.noreply.github.com>
2024-09-01 21:27:40 +0200

    Make migratedServer a boolean (#20)

--- src/configurationProvider.ts
+++ src/configurationProvider.ts
@@ -3,7 +3,7 @@ export const configuration = {
     backendUrl: import.meta.env.VITE_SERVER_ACCESS_TOKEN,
     newBackendUrl: import.meta.env.VITE_NEW_SERVER_ACCESS_TOKEN,
   },
-  migratedServer: import.meta.env.VITE_USE_NEW_SERVER,
+  migratedServer: import.meta.env.VITE_USE_NEW_SERVER === "true",
   map: {
     defaultViewportSettingsNYC: {
       latitude: 40.736,
